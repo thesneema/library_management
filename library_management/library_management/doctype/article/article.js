8317fbd0cd10cdf48107b768f424373567ac78f3
Thesneema M <thezniz123@gmail.com>
2024-05-28 16:23:43 +0530

    fix:uniques ID to the each copy of same article

--- library_management/library_management/doctype/article/article.js
+++ library_management/library_management/doctype/article/article.js
@@ -1,3 +1,43 @@
+frappe.ui.form.on('Article', {
+    issue_copy: function(frm) {
+        frappe.call({
+            method: "your_app.article.article.issue_copy",
+            args: {
+                docname: frm.doc.name
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frm.refresh();
+                }
+            }
+        });
+    },
+    return_copy: function(frm) {
+        frappe.prompt({
+            fieldname: 'unique_id',
+            label: 'Unique ID',
+            fieldtype: 'Data',
+            reqd: 1
+        },
+        function(values){
+            frappe.call({
+                method: "your_app.article.article.return_copy",
+                args: {
+                    docname: frm.doc.name,
+                    unique_id: values.unique_id
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.refresh();
+                    }
+                }
+            });
+        },
+        __('Return Copy'),
+        __('Submit')
+        );
+    }
+});
 // Copyright (c) 2024, thesni and contributors
 // For license information, please see license.txt
 
