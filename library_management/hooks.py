fb332f9070f9b74f20811d19365aacef052ae0bc
Thesneema M <thezniz123@gmail.com>
2024-05-29 15:04:21 +0530

    fix: correct the issues of transaction

--- library_management/hooks.py
+++ library_management/hooks.py
@@ -122,13 +122,14 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+    "User": {
+        "after_insert": "library_management.library_management.doc_events.user.user.new_user_document",
+        "validate": "library_management.library_management.doc_events.user.user.validate_single_librarian_role"
+    }
+}
+
+
 
 # Scheduled Tasks
 # ---------------
@@ -226,4 +227,3 @@ app_license = "mit"
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
-
--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2024, thesni and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 
